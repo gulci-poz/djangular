964077195b3fd02ae293b34e903c422475359e49
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2017-02-16 11:52:12 +0100

    2017-02-16 http requests

---
+++ scrumboard/static/scrumboard/js/scrumboard.js
@@ -0,0 +1,39 @@
+(function () {
+    'use strict';
+
+    var app = angular.module('scrumboard', []);
+
+    app.config(function ($interpolateProvider) {
+        $interpolateProvider.startSymbol('{$');
+        $interpolateProvider.endSymbol('$}');
+    });
+
+    app.controller('ScrumboardController',
+        ['$scope', '$http', ScrumboardController]);
+
+    function ScrumboardController($scope, $http) {
+        $scope.add = function (list, title) {
+            var card = {
+                list: list.id,
+                title: title
+            };
+
+            $http.post('/scrumboard/cards/', card)
+                .then(function (response) {
+                        list.cards.push(response.data);
+                    },
+                    function () {
+                        alert('Could not create card');
+                    }
+                );
+        };
+
+        $scope.data = [];
+        $http.get('/scrumboard/lists/').then(
+            function (response) {
+                $scope.data = response.data;
+            }
+        );
+    }
+
+}());
