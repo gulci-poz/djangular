86ec7ee298bd214d47b0d7e71fd9e0ff0bb70c32
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2017-02-17 14:10:18 +0100

    2017-02-16 working on authentication

--- djangular_front_production/scrumboard/directives/card.directive.js
+++ djangular_front_production/scrumboard/directives/card.directive.js
@@ -9,19 +9,27 @@
         return {
             templateUrl: '/scrumboard/directives/card.html',
             restrict: 'E',
-            controller: ['$scope', '$http', function($scope, $http) {
+            controller: ['$scope', '$http', function ($scope, $http) {
                 var url = '/api/scrumboard/cards/' + $scope.card.id + '/';
                 $scope.update = function () {
-                    $http.put(url, $scope.card);
+                    $http.put(url, $scope.card)
+                        .then(function () {
+
+                            },
+                            function () {
+                                console.log('Access denied: put')
+                            });
                 };
 
                 $scope.delete = function () {
-                    $http.delete(url).then(
-                        function () {
-                            var cards = $scope.list.cards;
-                            cards.splice(cards.indexOf($scope.card), 1);
-                        }
-                    );
+                    $http.delete(url)
+                        .then(function () {
+                                var cards = $scope.list.cards;
+                                cards.splice(cards.indexOf($scope.card), 1);
+                            },
+                            function () {
+                                console.log('Access denied: delete')
+                            });
                 };
 
                 $scope.modelOptions = {
--- djangular_front_production/scrumboard/js/scrumboard.js
+++ djangular_front_production/scrumboard/js/scrumboard.js
@@ -4,11 +4,11 @@
     var app = angular.module('scrumboard', []);
 
     /*
-    app.config(function ($interpolateProvider) {
-        $interpolateProvider.startSymbol('{$');
-        $interpolateProvider.endSymbol('$}');
-    });
-    */
+     app.config(function ($interpolateProvider) {
+     $interpolateProvider.startSymbol('{$');
+     $interpolateProvider.endSymbol('$}');
+     });
+     */
 
     app.controller('ScrumboardController',
         ['$scope', '$http', ScrumboardController]);
@@ -25,32 +25,45 @@
                         list.cards.push(response.data);
                     },
                     function () {
-                        alert('Could not create card');
+                        console.log('Could not create card: post');
                     }
                 );
+        };
+
+        $scope.login = function () {
+            $http.post('/api/auth_api/login/',
+                {username: 'gulci', password: 'gulci'})
+                .then(function () {
+
+                }, function () {
+                    console.log('Access denied: post login')
+                });
         };
 
         $scope.data = [];
 
         $scope.loadData = function () {
-            $http.get('/api/scrumboard/lists/').then(
-                function (response) {
-                    $scope.data = response.data;
-                }
-            );
+            $http.get('/api/scrumboard/lists/')
+                .then(function (response) {
+                        $scope.data = response.data;
+                    },
+                    function () {
+                        console.log('Access denied: get')
+                    }
+                );
         };
 
         // wymaga ngRoute -> todo_later
         // musimy pamiętać o $route w definicji i funkcji kontrolera
         /*
-        $scope.reloadRoute = function () {
-            $route.reload();
-        };
+         $scope.reloadRoute = function () {
+         $route.reload();
+         };
 
-        $scope.$watch("data", function () {
-            $scope.reloadRoute();
-        });
-        */
+         $scope.$watch("data", function () {
+         $scope.reloadRoute();
+         });
+         */
 
         // początkowe ładowanie danych
         $scope.loadData();
